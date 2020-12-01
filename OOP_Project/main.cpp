eefe975326086648636b07fb7927c9d7c5f30356
VictorPuga <39507381+VictorPuga@users.noreply.github.com>
2020-12-01 15:52:07 -0700

    Initial Commit

---
+++ OOP_Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP_Project
+//
+//  Created by Víctor Manuel Puga Ruiz on 01/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
